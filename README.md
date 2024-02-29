1265b8768ae4be4f4485ab8bb750b4325d6c1bc4
DeemasDee <deemasdee@gmail.com>
2024-02-29 21:39:05 +0700

    update v.0.1.10

---
+++ README.md
@@ -0,0 +1,20 @@
+# Streamlit Dashboard ✨
+
+## Setup environment
+```
+pip install panda plotly streamlit
+```
+
+## Run steamlit app
+```
+streamlit run dashboard.py
+```
+
+# Panduan
+
+Untuk menjalankan dashboard sangatlah mudah.
+terdapat panah ' > ' di pojok kiri layar. apabila di klik, akan menampilkan panel side bar.
+
+panel side bar kiri, terdapat profil dan opsi singkat untuk menampilkan isi
+Dataset, dan Summery statics.
+apabila tidak dicentang, hanya menampilkan isi dashboard sederhana.
--- streamlit/dashboard.py
+++ streamlit/dashboard.py
@@ -10,29 +10,23 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-# ==============================
-# LOAD DATA
-# ==============================
 
+# LOAD DATA
 
 @st.cache_resource
 def load_data():
-    data = pd.read_csv("../dataset/hour.csv")
+    data = pd.read_csv("./dataset/hour.csv")
     return data
 
 
 data = load_data()
 
 
-# ==============================
 # TITLE DASHBOARD
-# ==============================
 # Set page title
 st.title("Bike Share Dashboard")
 
-# ==============================
 # SIDEBAR
-# ==============================
 st.sidebar.title("Contact Person:")
 st.sidebar.markdown("**• DEE'MAS A. KHOIRI RAHMANDANI**")
 st.sidebar.markdown(
@@ -66,9 +60,7 @@ st.sidebar.markdown('3: Salju Ringan, Hujan Ringan + Badai Petir + Awan Tersebar
 st.sidebar.markdown('4: Hujan Lebat + Palet Es + Badai Petir + Kabut, Salju + Kabut')
 
 
-# ==============================
 # VISUALIZATION
-# ==============================
 
 # create a layout with two columns
 col1, col2 = st.columns(2)
@@ -83,7 +75,7 @@ with col1:
 
     season_count = data.groupby("season_label")["total_count"].sum().reset_index()
     fig_season_count = px.bar(season_count, x="season_label",
-                              y="cnt", title="Jumlah Penyewa berdasarkan Musim")
+                              y="total_count", title="Jumlah Penyewa berdasarkan Musim")
     st.plotly_chart(fig_season_count, use_container_width=True,
                     height=400, width=600)
 
@@ -93,7 +85,7 @@ with col2:
 
     weather_count = data.groupby("weather_condition")["total_count"].sum().reset_index()
     fig_weather_count = px.bar(weather_count, x="weather_condition",
-                               y="cnt", title="Jumlah Penyewa berdasar Cuaca")
+                               y="total_count", title="Jumlah Penyewa berdasar Cuaca")
     # Mengatur tinggi dan lebar gambar
     st.plotly_chart(fig_weather_count, use_container_width=True,height=400, width=800)
 
